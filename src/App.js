187f3bd6573236011db5897ddc2b5b80cbd1fb6c
mfranke84 <60253062+mfranke84@users.noreply.github.com>
2021-02-17 07:24:55 +0100

    Updates to image

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import Navigation from './Components/Navigation/Navigation'
 import Logo from './Components/Logo/Logo'
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm'
 import Rank from './Components/Rank/Rank'
+import FaceRegocnition from './Components/FaceRegocnition/FaceRegocnition'
 import Particles from 'react-particles-js';
 import './App.css';
+import { Component } from 'react';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({apiKey: '1d90ddbaf08f4aa08d0efc2e8470061d'});
 
 const particlesOptions ={
   particles: {
@@ -17,19 +22,44 @@ const particlesOptions ={
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles' 
-        params={particlesOptions}
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { 
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState( {input: event.target.value} );
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input, {video: false})
+      .then(response => {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      })
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <Particles className='particles' 
+          params={particlesOptions}
+          />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit} 
         />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRegocnition />  */}
-    </div>
-  );
+        <FaceRegocnition onButtonSubmit={this.onButtonSubmit}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/FaceRegocnition/FaceRegocnition.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const FaceRegocnition = ({onButtonSubmit}) => {
+    return (
+        <div>
+            <image src=''></image>
+        </div>
+    );
+}
+
+export default FaceRegocnition;
