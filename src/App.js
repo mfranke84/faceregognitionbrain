508e66bc0f65f93dea0801e71b787051b8b9645e
Markus Franke <markus.dirk.franke@volkswagen-infotainment.com>
2021-02-22 18:33:07 +0100

    started face box

--- src/App.js
+++ src/App.js
@@ -27,7 +27,8 @@ class App extends Component {
     super();
     this.state = { 
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {},
     }
   }
 
@@ -36,12 +37,30 @@ class App extends Component {
     console.log( event.target.value)
   }
 
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    console.log(clarifaiFace);
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width,height)
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
     app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input, {video: false})
       .then(response => {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      })
+        this.calculateFaceLocation(response)
+        
+  
+      }) 
+      .catch(err => console.log(err))
   }
 
   render () {
--- src/Components/FaceRegocnition/FaceRegocnition.js
+++ src/Components/FaceRegocnition/FaceRegocnition.js
@@ -4,7 +4,7 @@ import React from 'react'
 const FaceRegocnition = ({imageUrl}) => {
     return (
         <div className='center'>
-            <img alt='' src={imageUrl} width='500px' height='auto'></img>
+            <img id='inputImage' alt='' src={imageUrl} width='500px' height='auto'></img>
         </div>
     );
 }
