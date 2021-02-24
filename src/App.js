cb717042630eafe66f34e74cb7cb733a49d612f3
Markus Franke <markus.dirk.franke@volkswagen-infotainment.com>
2021-02-24 10:57:28 +0100

    Completed box

--- src/App.js
+++ src/App.js
@@ -39,24 +39,28 @@ class App extends Component {
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    console.log(clarifaiFace);
     const image = document.getElementById('inputImage');
     const width = Number(image.width);
     const height = Number(image.height);
-    console.log(width,height)
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
       rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height)
+      bottomRow: height - (clarifaiFace.bottom_row * height) 
     }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box)
+    this.setState({box: box})
+    
   }
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
     app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input, {video: false})
       .then(response => {
-        this.calculateFaceLocation(response)
+        this.displayFaceBox( this.calculateFaceLocation(response) )
         
   
       }) 
@@ -76,7 +80,7 @@ class App extends Component {
           onInputChange={this.onInputChange} 
           onButtonSubmit={this.onButtonSubmit} 
         />
-        <FaceRegocnition imageUrl={this.state.imageUrl}/>
+        <FaceRegocnition box={this.state.box} imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
--- src/Components/FaceRegocnition/FaceRegocnition.js
+++ src/Components/FaceRegocnition/FaceRegocnition.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './FaceRegocnition.css';
 
 
-const FaceRegocnition = ({imageUrl}) => {
+const FaceRegocnition = ({imageUrl, box}) => {
     return (
-        <div className='center'>
-            <img id='inputImage' alt='' src={imageUrl} width='500px' height='auto'></img>
+        <div className='center ma'>
+            <div className='absolute mt2' >
+                <img id='inputImage' alt='' src={imageUrl} width='500px' height='auto'></img>
+                <div className='boundingBox' style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}></div>
+            </div>
         </div>
     );
 }
